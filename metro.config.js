4f9733d79d70df1522d53a3e58ec13b8199ceba8
DashaMyagkova <amia.dasha1@gmail.com>
2024-07-16 20:18:59 +0300

    feat: added svgs & svg-transformer

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,25 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const {
+  resolver: { assetExts, sourceExts },
+} = getDefaultConfig(__dirname);
+
+const config = {
+  resolver: {
+    assetExts: assetExts.filter((ext) => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+  },
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
+
+module.exports = mergeConfig(defaultConfig, config);
